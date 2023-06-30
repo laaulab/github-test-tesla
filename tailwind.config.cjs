759d4836ff87f73fcb3a2c1486e460e7fcf7e458
Laura Rodriguez <laura.rdsz97@gmail.com>
2023-06-30 12:20:32 +0200

    INITIAL COMMIT

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./src/**/*.{astro,sass,scss,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	 	'./assets/**/*.{sass,scss,css}'
+	],
+	theme: {
+		fontFamily:{
+			sans: ['Gotham SSm A', 'sans-serif']
+		},
+		extend: {},
+	},
+	plugins: [],
+}
